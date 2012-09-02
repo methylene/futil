13b13ff4a64a3a84097a19f5fed98238efb1a100
methylene <theairspeedindicator@yahoo.com>
2012-09-02 19:54:19 +0000

    this is now a phase listener

--- src/main/java/org/meth4j/futil/MessageFilter.java
+++ src/main/java/org/meth4j/futil/MessageFilter.java
@@ -1,43 +1,32 @@
 package org.meth4j.futil;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.meth4j.futil.Messages;
-import org.slf4j.LoggerFactory;
+public class MessageFilter implements PhaseListener {
 
-public class MessageFilter implements Filter {
+	private static final long serialVersionUID = 1L;
 
-	public void doFilter(final ServletRequest req, final ServletResponse resp, final FilterChain chain)
-			throws IOException, ServletException {
-		final HttpServletRequest r = (HttpServletRequest) req;
-		final HttpServletResponse response = (HttpServletResponse) resp;
-		final String servletPath = r.getServletPath();
-		if (servletPath.startsWith("/javax.faces.resource")) {
-			chain.doFilter(req, resp);
-		} else {
-			Messages.restoreMessage(r);
-			chain.doFilter(req, resp);
-			if (response.getStatus() == HttpServletResponse.SC_OK) {
-				Messages.clearMessage(r);
-			}
-		}
+	@Override public void afterPhase(PhaseEvent event) {
+		FacesContext fc = event.getFacesContext();
+		ExternalContext ec = fc.getExternalContext();
+		final HttpServletRequest r = (HttpServletRequest) ec.getRequest();
+		Messages.clearMessage(r);
 	}
 
-	public void init(final FilterConfig filterConfig) throws ServletException {
-		LoggerFactory.getLogger(getClass()).info("{} starting up...", getClass().getSimpleName());
+	@Override public void beforePhase(PhaseEvent event) {
+		FacesContext fc = event.getFacesContext();
+		ExternalContext ec = fc.getExternalContext();
+		final HttpServletRequest r = (HttpServletRequest) ec.getRequest();
+		Messages.restoreMessage(r);
 	}
 
-	public void destroy() {
-		LoggerFactory.getLogger(getClass()).info("{} shutting down...", getClass().getSimpleName());
+	@Override public PhaseId getPhaseId() {
+		return PhaseId.RENDER_RESPONSE;
 	}
 
 }
