6b8c8f8fe92fcc85a2c0a5bd32681f3009d77b3d
methylene <theairspeedindicator@yahoo.com>
2012-09-02 19:53:39 +0000

    use facesmessage objects instead of homebrew

--- src/main/java/org/meth4j/futil/Message.java
+++ src/main/java/org/meth4j/futil/Message.java
@@ -2,56 +2,25 @@ package org.meth4j.futil;
 
 import java.io.Serializable;
 
-import com.google.common.base.Preconditions;
+import javax.faces.application.FacesMessage;
 
 public class Message implements Serializable {
 
-	public static enum Severity {
-		
-//		@formatter:off
-		ERROR,
-		WARN,
-		INFO,
-		INVALID_FIELDS,
-;//		@formatter:on
-		
+	private Message() {
 	}
 
 	private static final long serialVersionUID = 2L;
 
-	private final IKey message;
-	private final Severity severity;
-
-	private Message(IKey message, Severity severity) {
-		super();
-		Preconditions.checkNotNull(message);
-		Preconditions.checkNotNull(severity);
-		this.message = message;
-		this.severity = severity;
+	public static final FacesMessage info(IKey message) {
+		return new FacesMessage(FacesMessage.SEVERITY_INFO, message.getLabel(), message.getLabel());
 	}
 
-	public String getSeverity() {
-		return severity.name().toLowerCase();
+	public static final FacesMessage warn(IKey message) {
+		return new FacesMessage(FacesMessage.SEVERITY_WARN, message.getLabel(), message.getLabel());
 	}
 
-	public static final Message info(IKey message) {
-		return new Message(message, Severity.INFO);
-	}
-
-	public static final Message warn(IKey message) {
-		return new Message(message, Severity.WARN);
-	}
-
-	public static final Message error(IKey message) {
-		return new Message(message, Severity.ERROR);
-	}
-	
-	public static final Message invalidFields(IKey message) {
-		return new Message(message, Severity.INVALID_FIELDS);
-	}
-
-	public String getMessage() {
-		return message.getLabel();
+	public static final FacesMessage error(IKey message) {
+		return new FacesMessage(FacesMessage.SEVERITY_ERROR, message.getLabel(), message.getLabel());
 	}
 
 }
--- src/main/java/org/meth4j/futil/Messages.java
+++ src/main/java/org/meth4j/futil/Messages.java
@@ -3,24 +3,19 @@ package org.meth4j.futil;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import org.slf4j.LoggerFactory;
 
 public class Messages {
 
 	private static final String MESSAGE = "org.meth4j.futil.message";
 	private static final String BUNDLE_NAME = "org.meth4j.futil.messages";
 
-	public static final void add(Message message, HttpServletRequest r) {
-		if (getMessage(r) != null) {
-			LoggerFactory.getLogger(Messages.class).warn("add: Overwriting existing message {} with {}",
-					getMessage(r).toString(), message.toString());
-		}
-		r.setAttribute(MESSAGE, message);
+	public static final void add(FacesMessage message) {
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 
 	public static ResourceBundle getBundle() {
@@ -31,31 +26,21 @@ public class Messages {
 		return ResourceBundle.getBundle(BUNDLE_NAME, l);
 	}
 
-	public static void add(Message message) {
-		FacesContext fc = FacesContext.getCurrentInstance();
-		HttpServletRequest r = (HttpServletRequest) fc.getExternalContext().getRequest();
-		add(message, r);
-	}
-
-	public static void flash(Message message, HttpServletRequest r) {
+	public static void flash(FacesMessage message, HttpServletRequest r) {
 		r.getSession().setAttribute(MESSAGE, message);
 	}
 
-	public static void flash(Message message) {
+	public static void flash(FacesMessage message) {
 		FacesContext fc = FacesContext.getCurrentInstance();
 		HttpServletRequest r = (HttpServletRequest) fc.getExternalContext().getRequest();
 		flash(message, r);
 	}
 
-	private static Message getMessage(HttpServletRequest r) {
-		return (Message) r.getAttribute(MESSAGE);
-	}
-
-	public static Message restoreMessage(HttpServletRequest r) {
+	public static FacesMessage restoreMessage(HttpServletRequest r) {
 		HttpSession s = r.getSession();
-		Message message = (Message) s.getAttribute(Messages.MESSAGE);
-		if (s != null) {
-			add(message, r);
+		FacesMessage message = (FacesMessage) s.getAttribute(MESSAGE);
+		if (message != null) {
+			add(message);
 		}
 		return message;
 	}
