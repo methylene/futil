0c425ae8be1ac3d98a0502cfdaaff39baecb96f0
methylene <theairspeedindicator@yahoo.com>
2012-09-03 21:58:39 +0000

    add validation methods

--- src/main/java/org/meth4j/futil/Validate.java
+++ src/main/java/org/meth4j/futil/Validate.java
@@ -1,5 +1,8 @@
 package org.meth4j.futil;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import com.google.common.primitives.Ints;
 
 public class Validate {
@@ -18,5 +21,38 @@ public class Validate {
 		}
 		return value;
 	}
-
+	
+	public static final void required(String value) throws ValidatorException {
+		if (value == null || value.isEmpty()) {
+			throw new ValidatorException(CommonKey.ERROR_REQUIRED);
+		}
+	}
+	
+	public static final void maxLength(String value, int maxLength) throws ValidatorException {
+		if (value != null) {
+			if (value.length() > maxLength) {
+				throw new ValidatorException(CommonKey.ERROR_TOO_LONG);
+			}
+		}
+	}
+	
+	public static final void minLength(String value, int minLength) throws ValidatorException {
+		if (value != null) {
+			if (value.length() < minLength) {
+				throw new ValidatorException(CommonKey.ERROR_TOO_SHORT);
+			}
+		}
+	}
+	
+	public static final void emailPattern(String value) throws ValidatorException {
+		if (value != null) {
+			try {
+				InternetAddress ia = new InternetAddress(value);
+				ia.validate();
+			} catch (AddressException e) {
+				throw new ValidatorException(CommonKey.ERROR_EMAIL_PATTERN);
+			}
+		}
+	}
+	
 }
