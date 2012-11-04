9d76f3fc2df336af574930624d86a1dd8d014fc3
methylene <theairspeedindicator@yahoo.com>
2012-11-04 09:49:02 +0000

    version 0.2.1 ValidatingConverter is now a javax.faces.validator.Validator Must now set javax.faces.VALIDATE_EMPTY_FIELDS to true in web.xml

--- src/main/java/org/meth4j/futil/Validate.java
+++ src/main/java/org/meth4j/futil/Validate.java
@@ -1,37 +1,52 @@
 package org.meth4j.futil;
 
+import static com.google.common.primitives.Ints.tryParse;
+import static org.meth4j.futil.CommonKey.ERROR_INTEGER;
+import static org.meth4j.futil.CommonKey.ERROR_POSITIVE_NATURAL_NUMBER;
+import static org.meth4j.futil.CommonKey.ERROR_REQUIRED;
+import static org.meth4j.futil.Message.errMesg;
+
+import javax.faces.validator.ValidatorException;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-
-import com.google.common.primitives.Ints;
 
 public class Validate {
 	
 	public static Integer integer(String value) {
-		Integer num = Ints.tryParse(value.toString());
+		final Integer num = tryParse(value);
 		if (num == null || num < 1) {
-			throw new ValidatorException(CommonKey.ERROR_INTEGER);
+			throw new ValidatorException(errMesg(ERROR_INTEGER));
+		} else {
+			return num;
 		}
-		return num;
 	}
 	
 	public static Integer positive(Integer value) {
 		if (value < 1) {
-			throw new ValidatorException(CommonKey.ERROR_POSITIVE_NATURAL_NUMBER);
+			throw new ValidatorException(errMesg(ERROR_POSITIVE_NATURAL_NUMBER));
+		} else {
+			return value;
 		}
-		return value;
+	}
+	
+	public static final String requiredString(Object value) throws ValidatorException {
+		if (value == null || value.toString().isEmpty()) {
+			throw new ValidatorException(errMesg(ERROR_REQUIRED));
+		} else {
+			return value.toString();
+		}
 	}
 	
 	public static final void required(String value) throws ValidatorException {
 		if (value == null || value.isEmpty()) {
-			throw new ValidatorException(CommonKey.ERROR_REQUIRED);
+			throw new ValidatorException(errMesg(CommonKey.ERROR_REQUIRED));
 		}
 	}
 	
 	public static final void maxLength(String value, int maxLength) throws ValidatorException {
 		if (value != null) {
 			if (value.length() > maxLength) {
-				throw new ValidatorException(CommonKey.ERROR_TOO_LONG);
+				throw new ValidatorException(errMesg(CommonKey.ERROR_TOO_LONG));
 			}
 		}
 	}
@@ -39,7 +54,7 @@ public class Validate {
 	public static final void minLength(String value, int minLength) throws ValidatorException {
 		if (value != null) {
 			if (value.length() < minLength) {
-				throw new ValidatorException(CommonKey.ERROR_TOO_SHORT);
+				throw new ValidatorException(errMesg(CommonKey.ERROR_TOO_SHORT));
 			}
 		}
 	}
@@ -47,10 +62,10 @@ public class Validate {
 	public static final void emailPattern(String value) throws ValidatorException {
 		if (value != null) {
 			try {
-				InternetAddress ia = new InternetAddress(value);
+				final InternetAddress ia = new InternetAddress(value);
 				ia.validate();
-			} catch (AddressException e) {
-				throw new ValidatorException(CommonKey.ERROR_EMAIL_PATTERN);
+			} catch (final AddressException e) {
+				throw new ValidatorException(errMesg(CommonKey.ERROR_EMAIL_PATTERN));
 			}
 		}
 	}
